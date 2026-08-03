f8accf7d2e32009c536d62fc0a80eb66790fd0c8
NexBite <adityasolutioninfo@gmail.com>
2026-08-03 17:09:24 +0530

    Day 03: Learn JavaScript data types

---
+++ 01-javascript-fundamentals/day-03-data-types/mini-project.js
@@ -0,0 +1,35 @@
+// =======================  Mini-project — Developer Profile
+// Day 03 Mini-project: Developer Profile
+
+const developerName = "Aditya";
+const developerAge = 30;
+const isLearningJavaScript = true;
+
+let currentCompany;
+const currentJob = "Nill";
+
+const developerId = Symbol("developerId");
+const totalCodeLines = 1000000000000000000n;
+
+console.log("--------------------- Developer Profile ---------------------");
+
+console.log("Name:", developerName);
+console.log("Name type" , typeof developerName);
+
+console.log("\nAge:", developerAge);
+console.log("Age type" , typeof developerAge);
+
+console.log("\nIs Learning JavaScript:", isLearningJavaScript);
+console.log("Is Learning JavaScript type" , typeof isLearningJavaScript);
+
+console.log("\nCurrent Company:", currentCompany);
+console.log("Current Company type" , typeof currentCompany);
+
+console.log("\nCurrent Job:", currentJob);
+console.log("Current Job type" , typeof currentJob);
+
+console.log("\nDeveloper ID:", developerId);
+console.log("Developer ID type:", typeof developerId);
+
+console.log("\nTotal code lines:", totalCodeLines);
+console.log("Code lines type:", typeof totalCodeLines);
