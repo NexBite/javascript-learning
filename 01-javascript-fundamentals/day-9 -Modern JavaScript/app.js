94c4de7c348aa26fda164e751a57229ad1928c7a
NexBite <adityasolutioninfo@gmail.com>
2026-08-15 12:23:56 +0530

    Day 09 Modern Javascritps

---
+++ 01-javascript-fundamentals/day-9 -Modern JavaScript/app.js
@@ -0,0 +1,6 @@
+import { add, subtract } from "./math.js"; 
+import multiply from "./math.js";
+
+console.log(add(10,5));
+console.log(subtract(10,20));
+console.log(multiply(5 ,4));
---
+++ 01-javascript-fundamentals/day-9 -Modern JavaScript/mini-task.js
@@ -0,0 +1,10 @@
+const product = {
+  care : "Two wheeler",
+  brand : "Hero Honda",
+  price : 95000
+};
+console.log(
+`product : ${product.care} 
+  Brand : ${product.care}
+  Price : ${product.price}`
+);
